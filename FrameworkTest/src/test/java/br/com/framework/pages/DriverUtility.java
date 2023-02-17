891990b8f712bbca835cdbba932bf7fadbd4aa5f
Enzo Diogo de Santana Oliveira <72278152+EnzoD07@users.noreply.github.com>
2023-02-17 09:47:21 -0300

    Update DriverUtility.java

--- FrameworkTest/src/test/java/br/com/framework/pages/DriverUtility.java
+++ FrameworkTest/src/test/java/br/com/framework/pages/DriverUtility.java
@@ -1,20 +1,34 @@
 package br.com.framework.pages;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverUtility {
 
-	WebDriver driver;
+	private WebDriver driver;
+	WebDriverManager.chromedriver().setup();
+	ChromeOptions options = new ChromeOptions();
+	options.addArguments("--no-sandbox");
+	options.addArguments("--disable-dev-shm-usage");
+	options.addArguments("--headless");
+	driver = new ChromeDriver(options);
+	driver.navigate().to("https://www.google.com");
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
+//	WebDriver driver;
 
-	public WebDriver navegador(String url) {
-		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("headless");
-		driver = new ChromeDriver();
-		driver.get(url);
-		driver.manage().window().maximize();
-		return driver;
-	}
+//	public WebDriver navegador(String url) {
+//		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("headless");
+//		driver = new ChromeDriver();
+//		driver.get(url);
+//		driver.manage().window().maximize();
+//		return driver;
+//	}
 
 }
